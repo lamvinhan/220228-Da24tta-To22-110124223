5b1caf41ad8d740dca4f02ec355f99692f63eaf6
lamvinhan <lamvinhan02@gmail.com>
2024-11-14 10:35:51 +0700

    Add files via upload

---
+++ vinhanbt1`.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	long s;
+	printf("\nNhap vao so nguyen n:");
+	scanf("%d",&n);
+	 for (int i=1 ;i<=n;i++)
+	 {
+	 	s=s+i;
+	 	
+	 }
+	printf(" tong la %d :",s);
+	 
+	 return 0;
+}
