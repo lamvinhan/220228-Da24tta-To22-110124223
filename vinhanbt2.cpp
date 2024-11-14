e9af58009ad8c5fddc3b6b57b43384f4a2d93363
lamvinhan <lamvinhan02@gmail.com>
2024-11-14 10:36:24 +0700

    Add files via upload

---
+++ vinhanbt2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	long p;
+printf("\n nhap vao so nguyen n :");
+	scanf("%d",&n);
+	for (int i=0  ; i<=n ; i++)
+	{
+		p=p*i;
+    }
+    printf("tich la %d :",p);
+     return 0;
+}
