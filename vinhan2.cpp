e8f9c00645a9238705920899eb05a601f8a713c7
lamvinhan <lamvinhan02@gmail.com>
2024-11-14 10:35:30 +0700

    Add files via upload

---
+++ vinhan2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	//Khai bao bien 
+	int a = 100;
+	float b = 8.95;
+	char c = 'C';
+	
+	//In gia tri cua bien ra man hinh 
+	printf(" so nguyen : %d\n ", a);
+	printf(" so thuc : %.2f\n",b);
+	printf(" ky tu : '%C'\n",c);
+	 
+	 return 0;
+}	  
+	
